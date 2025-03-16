24034e5c094a7a8109d8a198be0e1541326fffcb
GILBERTO-FROYLAN-FERNANDEZ <gilberff@uci.edu>
2025-03-16 00:06:59 +0000

    Implemented dijkstra's

---
+++ src/dijkstras.cpp
@@ -0,0 +1,74 @@
+#include "dijkstras.h"
+#include <utility>
+
+int calculate_cost(const vector<int>& path, const Graph& G){
+    int total = 0;
+    for (int i = 0; i < path.size() - 1; ++i){
+        for (Edge e : G[path[i]]){
+            if (e.dst == path[i + 1]){
+                total += e.weight;
+                break;
+            }
+        }
+    }
+    return total;
+}
+
+void print_path(const vector<int>& v, int total){
+    for (int val : v) cout << val << ' ';
+    cout << endl;
+}
+
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination){
+    vector<int> res;
+    int current = destination;
+    stack<int> temp;
+    while (current != 0){
+        temp.push(previous[current]);
+        current = previous[current];
+    }
+    while (!temp.empty()){
+        res.push_back(temp.top());
+        temp.pop();
+    }
+    res.push_back(destination);
+    return res;
+}
+
+vector<int>& dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous){
+    int n = G.size();
+    int dist[n];
+    previous.assign(n, -1);
+    previous[source] = 0;
+    bool visited[n];
+    for (int i = 0; i < n; ++i){
+        dist[i] = std::numeric_limits<int>::max();
+        visited[i] = false;
+    }
+    priority_queue<
+        pair<int, int>,
+        vector<pair<int, int>>,
+        greater<pair<int, int>> > pq;
+    
+    pq.push(pair{0, source});
+    dist[source] = 0;
+    
+    while (!pq.empty()){
+        pair current = pq.top(); pq.pop();
+        int u = current.second; // vertex
+        if (visited[u]) continue;
+        visited[u] = true;
+        for (Edge edge : G[u]){
+            int v = edge.dst;
+            int weight = edge.weight;
+            if (!visited[v] && dist[u] + weight < dist[v]){
+                dist[v] = dist[u] + weight;
+                previous[v] = u;
+                pq.push(pair{dist[v], v});
+            }
+        }
+    }
+    return previous;
+
+    
+}
---
+++ src/dijkstras_main.cpp
@@ -0,0 +1,20 @@
+#include "dijkstras.h"
+#include <numeric>
+
+int main(int argc, char* args[]){
+    Graph G;
+    string path = (argc > 1) ? args[1] : "../src/small.txt";
+    file_to_graph(path, G);
+    // if (argc > 1) file_to_graph(args[1], G);
+    // else file_to_graph("../src/small.txt", G);
+    // else {cerr << "Please input file name" << endl; return 0;}
+    vector<int> previous(G.size());
+    for (int i = 0; i < previous.size(); ++i){
+        dijkstra_shortest_path(G, 0, previous);
+        vector<int> path(extract_shortest_path({}, previous, i));
+        print_path(path, accumulate(path.begin(), path.end(), 0));
+        cout << "Total cost is " << calculate_cost(path, G) << endl;
+    }
+    // for (int val : value) cout << val;
+    // cout << endl;
+}
